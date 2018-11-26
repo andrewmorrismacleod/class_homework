f5293ca837d41cdea0115492b1ebf43ba9074706
Andrew MacLeod <andrewmorrismacleod@googlemail.com>
2018-11-26 23:15:22 +0000

    Class A - Add Second Test

--- specs/class_A_test.rb
+++ specs/class_A_test.rb
@@ -9,4 +9,9 @@ class TestStudent< MiniTest::Test
     assert_equal("Andrew", student.get_name)
   end
 
+  def test_cohort
+    student = Student.new("Andrew", "E27")
+    assert_equal("E27", student.get_cohort)
+  end
+
 end
